90862ba096ef7812d2db4696d317a578c5f1b600
seungyeop-lee <xbeast7@gmail.com>
2020-09-20 17:18:01 +0900

    add endpoint for file list

--- server/main.go
+++ server/main.go
@@ -4,6 +4,8 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/render"
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
 func main() {
@@ -11,6 +13,23 @@ func main() {
 
 	e.GET("/ping", func(c *gin.Context) {
 		c.Render(http.StatusOK, render.String{Format: "Pong"})
+	})
+
+	e.GET("/list", func(c *gin.Context) {
+		var files []string
+		if err := filepath.Walk(
+			"/app/file-share/share_dir",
+			func(path string, info os.FileInfo, err error) error {
+				if !info.IsDir() {
+					files = append(files, path)
+				}
+				return nil
+			},
+		); err != nil {
+			c.Render(http.StatusInternalServerError, render.String{Format: "can't find share dir"})
+			return
+		}
+		c.JSON(http.StatusOK, files)
 	})
 
 	if err := e.Run(":8080"); err != nil {
